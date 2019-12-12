8f2d39dc04a0d3e3d1f708095b5a05943695ca33
Eric Hicks <ehicks05@yahoo.com>
2019-12-12 16:02:48 -0500

    Add day 9 solution (kotlin).

--- src/main/java/net/ehicks/advent/year2019/Day09.kt
+++ src/main/java/net/ehicks/advent/year2019/Day09.kt
@@ -10,13 +10,8 @@ fun main() {
             .split(",").stream().mapToLong { it.toLong() }.toList()
 
     val mem = input.mapIndexed { index, i -> index.toLong() to i }.toMap()
-
-    val test = listOf<Long>(109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99)
-    val testMem = test.mapIndexed { index, i -> index.toLong() to i }.toMap()
-
-    println("Test 1: " + solvePart1(testMem))
     println("Part 1: " + solvePart1(mem))
-    println("Part 2: " + solvePart2(input))
+    println("Part 2: " + solvePart2(mem))
 }
 
 private fun solvePart1(inputProgram: Map<Long, Long>): Long {
@@ -24,58 +19,55 @@ private fun solvePart1(inputProgram: Map<Long, Long>): Long {
     return programState.output.first()
 }
 
-private fun solvePart2(inputProgram: List<Long>): Long {
-    return 0
+private fun solvePart2(inputProgram: Map<Long, Long>): Long {
+    val programState = runProgramUntilHalt(inputProgram, listOf(2))
+    return programState.output.first()
 }
 
-private fun runProgramUntilHalt(inputProgram: Map<Long, Long>, input: List<Long>): ProgramState {
+private fun runProgramUntilHalt(inputProgram: Map<Long, Long>, input: List<Long>): MachineState {
     val program = inputProgram.toMutableMap()
 
-    var programState = ProgramState(program, 0, input, 0, mutableListOf())
+    var programState = MachineState(program, 0, input, 0, mutableListOf())
     while (programState.mem[programState.programIndex] != 99L)
         programState = processOpCode(programState)
 
     return programState
 }
 
-data class ProgramState(val mem: MutableMap<Long, Long>, var programIndex: Long, val input: List<Long>, var inputIndex: Int,
+data class MachineState(val mem: MutableMap<Long, Long>, var programIndex: Long, val input: List<Long>, var inputIndex: Int,
                         val output: MutableList<Long>, var halted: Boolean = false, var relativeBase: Long = 0)
 
-private fun processOpCode(programState: ProgramState): ProgramState {
-    val program = programState.mem
-    val index = programState.programIndex
+private fun processOpCode(machineState: MachineState): MachineState {
+    val memory = machineState.mem
+    val index = machineState.programIndex
 
-    val instruction = program[index].toString().reversed()
+    val instruction = memory[index].toString().reversed()
     val opCode = Integer.parseInt(instruction.substring(0, 1))
     val paramModes = getParamModes(instruction)
 
-    val param1 = getParam(program, index, paramModes.first, 1L, programState.relativeBase)
-    val param2 = getParam(program, index, paramModes.second, 2L, programState.relativeBase)
-    val param3 = getParam(program, index, paramModes.third, 3L, programState.relativeBase)
+    var isWrite = false
+    if (opCode == 3) isWrite = true
+
+    val param1 = getParam(memory, index, machineState.relativeBase, isWrite, 1L, paramModes.first)
+    val param2 = getParam(memory, index, machineState.relativeBase, isWrite, 2L, paramModes.second)
+    val param3 = getParam(memory, index, machineState.relativeBase, true, 3L, paramModes.third)
 
     val updatedProgramIndex = when (opCode) {
-        1 -> add(program, index, param1, param2, param3)
-        2 -> mult(program, index, param1, param2, param3)
-        3 -> input(program, index, param1, programState.input[programState.inputIndex])
-        4 -> {
-            val (updatedProgramIndex, output) = output(program, index, param1)
-            programState.output.add(output)
-            updatedProgramIndex
-        }
+        1 -> add(memory, index, param1, param2, param3)
+        2 -> mult(memory, index, param1, param2, param3)
+        3 -> input(memory, index, param1, machineState.input[machineState.inputIndex])
+        4 -> output(index, param1, machineState.output)
         5 -> jumpIfTrue(index, param1, param2)
         6 -> jumpIfFalse(index, param1, param2)
-        7 -> lessThan(program, index, param1, param2, param3)
-        8 -> equals(program, index, param1, param2, param3)
-        9 -> {
-            programState.relativeBase += param1
-            index + 2
-        }
+        7 -> lessThan(memory, index, param1, param2, param3)
+        8 -> equals(memory, index, param1, param2, param3)
+        9 -> relativeBaseOffset(index, param1, machineState)
         else -> 0
     }
 
-    val updatedInputIndex = if (opCode == 3) programState.inputIndex + 1 else programState.inputIndex
+    val updatedInputIndex = if (opCode == 3) machineState.inputIndex + 1 else machineState.inputIndex
 
-    return ProgramState(program, updatedProgramIndex, programState.input, updatedInputIndex, programState.output, programState.halted, programState.relativeBase)
+    return MachineState(memory, updatedProgramIndex, machineState.input, updatedInputIndex, machineState.output, machineState.halted, machineState.relativeBase)
 }
 
 private fun getParamModes(instruction: String): Triple<Int, Int, Int> {
@@ -85,23 +77,36 @@ private fun getParamModes(instruction: String): Triple<Int, Int, Int> {
     return Triple(param1Mode, param2Mode, param3Mode)
 }
 
-private fun getParam(data: MutableMap<Long, Long>, index: Long, paramMode: Int, offset: Long, relativeBase: Long): Long {
+private fun getParam(data: MutableMap<Long, Long>, index: Long, relativeBase: Long, isWrite: Boolean, offset: Long, paramMode: Int): Long {
     val rawAddress = data.getOrDefault(index + offset, 0)
-    return when (paramMode) {
-        0 -> data.getOrDefault(rawAddress, 0)
-        1 -> rawAddress
-        2 -> data.getOrDefault(relativeBase + rawAddress, 0)
-        else -> throw Exception("Invalid Param Mode")
-    }
-}
 
-private fun mult(data: MutableMap<Long, Long>, index: Long, param1: Long, param2: Long, param3Address: Long): Long {
-    data[param3Address] = param1 * param2
-    return index + 4
+    return when (isWrite) {
+        true -> {
+            when (paramMode) {
+                0 -> rawAddress
+                1 -> throw Exception("Cannot use immediate mode when writing")
+                2 -> relativeBase + rawAddress
+                else -> throw Exception("Invalid Param Mode")
+            }
+        }
+        false -> {
+            when (paramMode) {
+                0 -> data.getOrDefault(rawAddress, 0)
+                1 -> rawAddress
+                2 -> data.getOrDefault(relativeBase + rawAddress, 0)
+                else -> throw Exception("Invalid Param Mode")
+            }
+        }
+    }
 }
 
 private fun add(data: MutableMap<Long, Long>, index: Long, param1: Long, param2: Long, param3Address: Long): Long {
     data[param3Address] = param1 + param2
+    return index + 4
+}
+
+private fun mult(data: MutableMap<Long, Long>, index: Long, param1: Long, param2: Long, param3Address: Long): Long {
+    data[param3Address] = param1 * param2
     return index + 4
 }
 
@@ -110,9 +115,9 @@ private fun input(data: MutableMap<Long, Long>, index: Long, param1Address: Long
     return index + 2
 }
 
-private fun output(data: MutableMap<Long, Long>, index: Long, param1Address: Long): Pair<Long, Long> {
-    val output = data.getOrDefault(param1Address, 0)
-    return Pair(index + 2, output)
+private fun output(index: Long, param1: Long, machineOutput: MutableList<Long>): Long {
+    machineOutput.add(param1)
+    return index + 2
 }
 
 private fun jumpIfTrue(index: Long, param1: Long, param2: Long): Long {
@@ -131,4 +136,9 @@ private fun lessThan(data: MutableMap<Long, Long>, index: Long, param1: Long, pa
 private fun equals(data: MutableMap<Long, Long>, index: Long, param1: Long, param2: Long, param3Address: Long): Long {
     data[param3Address] = if (param1 == param2) 1L else 0
     return index + 4
+}
+
+private fun relativeBaseOffset(index: Long, param1: Long, machineState: MachineState): Long {
+    machineState.relativeBase += param1
+    return index + 2
 }
