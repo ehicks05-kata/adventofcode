ff6d68b6a8991c05e41d7584b2657a14e0efcb36
Eric Hicks <ehicks05@yahoo.com>
2020-04-28 13:49:12 -0400

    Changes to incomplete day 14 solution.

--- src/main/java/net/ehicks/advent/year2019/Day14.kt
+++ src/main/java/net/ehicks/advent/year2019/Day14.kt
@@ -6,7 +6,7 @@ import kotlin.math.ceil
 import kotlin.math.roundToInt
 
 fun main() {
-    val input = Files.readAllLines(Paths.get("src/main/resources/year2019/14c.txt"))
+    val input = Files.readAllLines(Paths.get("src/main/resources/year2019/14.txt"))
     val reactions = linesToReactions(input)
     println("Part 1: " + solvePart1(reactions))
     println("Part 2: " + solvePart2())
@@ -14,11 +14,12 @@ fun main() {
 
 private fun solvePart1(reactions: List<Reaction>): Int {
     val requirements = mutableListOf(ChemAmount("FUEL", 1))
+    val leftovers = mutableMapOf<String, Int>()
 
     var allRequirementsAreMadeWithOre = false
     while (!allRequirementsAreMadeWithOre) {
         // map chem amounts to their inputs until we're left with ore
-        val updatedRequirements = requirements.flatMap { getInputsForChemAmount(reactions, it, false) }.toMutableList()
+        val updatedRequirements = requirements.flatMap { getInputsForChemAmount(reactions, leftovers, it, false) }.toMutableList()
         val consolidatedRequirements = consolidateChemAmounts(updatedRequirements)
 
         requirements.clear()
@@ -27,10 +28,10 @@ private fun solvePart1(reactions: List<Reaction>): Int {
         allRequirementsAreMadeWithOre = requirements.all { isMadeWithOre(reactions, it) }
     }
 
-    val oreRequirements = requirements.flatMap { getInputsForChemAmount(reactions, it, true) }.toMutableList()
-
+    val oreRequirements = requirements.flatMap { getInputsForChemAmount(reactions, leftovers, it, true) }.toMutableList()
 
     // 321504 is too low
+    // 368024 is too high
     // 387756 is too high
     return oreRequirements.map { it.amount }.sum()
 }
@@ -41,7 +42,7 @@ private fun solvePart2() {
 
 private fun consolidateChemAmounts(chemAmounts: List<ChemAmount>): List<ChemAmount> {
     return chemAmounts.groupBy { it.name }
-            .map { ChemAmount(it.key, it.value.sumBy { chemAmount -> chemAmount.amount }) }
+            .map { ChemAmount(it.key, it.value.sumBy { chemAmount -> chemAmount.amount }, it.value[0].indent) }
 }
 
 private fun isMadeWithOre(reactions: List<Reaction>, chemAmount: ChemAmount): Boolean {
@@ -50,15 +51,30 @@ private fun isMadeWithOre(reactions: List<Reaction>, chemAmount: ChemAmount): Bo
     return reaction.inputs.size == 1 && reaction.inputs[0].name == "ORE"
 }
 
-private fun getInputsForChemAmount(reactions: List<Reaction>, chemAmount: ChemAmount, convertToOre: Boolean): List<ChemAmount> {
+private fun getInputsForChemAmount(reactions: List<Reaction>, leftovers: MutableMap<String, Int>,
+                                   chemAmount: ChemAmount, convertToOre: Boolean): List<ChemAmount> {
     val reaction = reactions.find { it.output.name == chemAmount.name } ?: return listOf(chemAmount)
 
-    if (!convertToOre)
-        if (reaction.inputs.size == 1 && reaction.inputs[0].name == "ORE") return listOf(chemAmount)
+    val previousLeftover = leftovers[chemAmount.name] ?: 0
+    val leftoversToUse = if (previousLeftover > chemAmount.amount) chemAmount.amount else previousLeftover
+    if (leftoversToUse > 0)
+        leftovers[chemAmount.name] = leftovers[chemAmount.name]!! - leftoversToUse
+    val amountToProduce = chemAmount.amount - leftoversToUse
 
-    val reactionServings = ceil(chemAmount.amount.toDouble() / reaction.output.amount).roundToInt().coerceAtLeast(1)
+    if (!convertToOre && reaction.inputs.size == 1 && reaction.inputs[0].name == "ORE")
+        return listOf(ChemAmount(chemAmount.name, chemAmount.amount - leftoversToUse))
 
-    return reaction.inputs.map { ChemAmount(it.name, it.amount * reactionServings) }
+    println(" ".repeat(chemAmount.indent * 2) + "Need ${reaction.inputs} to make $chemAmount")
+
+    if (amountToProduce != 0) {
+        val reactionServings = ceil(amountToProduce.toDouble() / reaction.output.amount).roundToInt().coerceAtLeast(1)
+        val leftover = reaction.output.amount * reactionServings % amountToProduce
+        leftovers.merge(chemAmount.name, leftover) {v1, v2 -> v1 + v2}
+
+        return reaction.inputs.map { ChemAmount(it.name, it.amount * reactionServings, chemAmount.indent + 1) }
+    }
+
+    return reaction.inputs.map { ChemAmount(it.name, 0, chemAmount.indent + 1) }
 }
 
 private fun linesToReactions(lines: List<String>): List<Reaction> {
@@ -76,5 +92,9 @@ private fun stringToChemAmount(input: String): ChemAmount {
     return ChemAmount(input.split(" ").last(), input.split(" ").first().toInt())
 }
 
-data class ChemAmount(val name: String, val amount: Int)
+data class ChemAmount(val name: String, val amount: Int, val indent: Int = 0) {
+    override fun toString(): String {
+        return "$amount $name"
+    }
+}
 data class Reaction(val inputs: List<ChemAmount>, val output: ChemAmount)
